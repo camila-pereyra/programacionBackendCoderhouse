b37dbff3fb1e3696c78f0e34b8174b7d7557c05d
camila.pereyra <campereyra23051995@gmail.com>
2023-06-20 20:25:24 -0300

    Desafio manejo de archivos

--- ProductManager.js
+++ ProductManager.js
@@ -6,7 +6,6 @@ class ProductManager {
     this.path = path;
   }
   addProduct(product) {
-    //VALIDACION: CAMPOS OBLIGATORIOS
     if (
       !product.title ||
       !product.description ||
@@ -15,37 +14,34 @@ class ProductManager {
       !product.code ||
       !product.stock
     ) {
-      return console.log(
+      console.log(
         "El producto que quiere agregar debe tener todos los campos completos"
       );
+      return;
     }
     //VALIDACION: EL PRODUCT ESTA (O NO) EN LA LISTA DE PRODUCTOS
     const found = this.products.find(
       (element) => element.code === product.code
     );
-    //Si lo encuentra retorna la leyenda que ya se encuentra el producto en la lista de productos
     if (found) {
       console.log(
         `El producto que quiere agregar (code: ${product.code}) ya se encuentra en la lista de productos`
       );
+      return;
     }
-    //Si no lo encuentra lo agrega...
-    if (!found) {
-      const productAdd = {
-        id: this.products.length + 1,
-        ...product,
-      };
-      this.products.push(productAdd);
-      //Escribo/sobreescribo en el archivo el array de productos
-      fs.promises
-        .writeFile(this.path, JSON.stringify(this.products, null, 2))
-        .then(() => console.log("Archivo grabado con éxito"))
-        .catch((error) => {
-          console.log("No se pudo grabar el archivo");
-          return error;
-        });
-    }
+    const productAdd = {
+      id: this.products.length + 1,
+      ...product,
+    };
+    this.products.push(productAdd);
+    this.grabarEnArchivo(this.products)
+      .then(() => console.log("Archivo grabado con éxito"))
+      .catch((error) => {
+        console.log("No se pudo grabar el archivo");
+        return error;
+      });
   }
+
   getProducts() {
     return new Promise((resolve, reject) => {
       fs.readFile(this.path, "utf8", (err, data) => {
@@ -62,22 +58,17 @@ class ProductManager {
       });
     });
   }
+
   getProductById(id) {
     return this.getProducts()
       .then((products) => {
         const indexProduct = products.findIndex((element) => element.id === id);
-        //Si lo encuentra...
-        if (indexProduct !== -1) {
-          console.log(
-            `El producto buscado (ID: ${id}):\n`,
-            products[indexProduct]
-          );
-        }
-        //Si no lo encuentra...
         if (indexProduct === -1) {
           console.log(
             `El producto que busca (id: ${id}) no se encontró en la lista de productos`
           );
+        } else {
+          return products[indexProduct];
         }
       })
       .catch((error) => {
@@ -88,27 +79,61 @@ class ProductManager {
     this.getProducts()
       .then((products) => {
         const indexProduct = products.findIndex((element) => element.id === id);
-        //Si lo encuentra...
-        if (indexProduct !== -1) {
-          products[indexProduct] = { id: id, ...product };
-          fs.promises
-            .writeFile(this.path, JSON.stringify(products, null, 2))
-            .then(() => console.log("Archivo actualizado con éxito"))
-            .catch((error) => {
-              console.log("No se pudo actualizar el archivo");
-              return error;
-            });
-        }
-        //Si no lo encuentra...
+
         if (indexProduct === -1) {
           console.log(
             `El producto que quiere actualizar (id: ${id}) no se encontró en la lista de productos`
           );
+          return;
+        }
+        const productUpdate = {
+          id: id,
+          ...product,
+        };
+        products[indexProduct] = productUpdate;
+        this.products = products;
+        this.grabarEnArchivo(this.products)
+          .then(() => console.log(`El producto ID.${id} actualizado con éxito`))
+          .catch((error) => {
+            console.log("No se pudo actualizar el archivo");
+            return error;
+          });
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
+  deleteProduct(id) {
+    this.getProducts()
+      .then((products) => {
+        const indexProduct = products.findIndex((element) => element.id === id);
+        if (indexProduct === -1) {
+          console.log(
+            `El producto que quiere eliminar (id: ${id}) no se encontró en la lista de productos`
+          );
+        } else {
+          products.splice(indexProduct, 1);
+          this.products = products;
+          this.grabarEnArchivo(products)
+            .then(() =>
+              console.log(`El producto ID.${id} fue eliminado exitosamente`)
+            )
+            .catch((error) => {
+              console.log("No se pudo eliminar el producto");
+              return error;
+            });
         }
       })
       .catch((error) => {
         return error;
       });
+  }
+  grabarEnArchivo(products) {
+    return fs.promises.writeFile(
+      this.path,
+      JSON.stringify(products, null, 2),
+      "utf-8"
+    );
   }
 }
 
@@ -125,12 +150,20 @@ productManager.addProduct({
   stock: 10,
 });
 productManager.addProduct({
-  title: "Tomate",
-  description: "Perita",
-  price: 130,
-  thumbnail: "tomate_perita.png",
-  code: 11150,
-  stock: 5,
+  title: "Arroz",
+  description: "1/2kg",
+  price: 150,
+  thumbnail: "arroz.png",
+  code: 18030,
+  stock: 20,
+});
+productManager.addProduct({
+  title: "Ravioles",
+  description: "2 planchas",
+  price: 500,
+  thumbnail: "ravioles.png",
+  code: 18010,
+  stock: 3,
 });
 
 //OBTENIENDO PRODUCTOS
@@ -144,23 +177,19 @@ productManager
   });
 
 //BUSCANDO PRODUCTOS POR ID
-productManager.getProductById(1); //ENCONTRADO
-productManager.getProductById(3); //NO ENCONTRADO
+productManager.getProductById(3).then((productFound) => {
+  productFound && console.log("Producto buscado: ", productFound);
+});
 
 //ACTUALIZANDO PRODUCTOS
-productManager.updateProduct(1, {
-  title: "Fideos tallarines",
-  description: "1/2kg",
-  price: 250,
-  thumbnail: "fideo_codito.png",
-  code: 18050,
-  stock: 10,
-});
-productManager.updateProduct(2, {
-  title: "Tomate perita",
-  description: "1kg",
-  price: 130,
-  thumbnail: "tomate_perita.png",
-  code: 11150,
+productManager.updateProduct(3, {
+  title: "Ravioles",
+  description: "Verdura",
+  price: 450,
+  thumbnail: "ravioles.png",
+  code: 18010,
   stock: 5,
 });
+
+//ELIMIMANDO PRODUCTO
+productManager.deleteProduct(5);
